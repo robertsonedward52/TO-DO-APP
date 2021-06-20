295c4dcc78d0ad7a9ad0e682e377d6bd0721846e
Edward D. Robertson <edwardd.robertson@Edwards-MacBook-Air.local>
2021-06-20 09:57:01 +0200

    third commit

---
+++ public/browser.js
@@ -0,0 +1,64 @@
+function itemTemplate(item) {
+    return  `
+    <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text"> ${item.text} </span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>
+    `
+}
+
+
+// Initial page reload render
+let OurHTML = items.map(function(item) {
+  return itemTemplate(item)
+}).join('')
+document.getElementById('item-list').insertAdjacentHTML('beforeend', OurHTML)
+
+// create feature
+let createField = document.getElementById('create-field')
+
+document.getElementById('create-form').addEventListener('submit', function(e) {
+    e.preventDefault()
+    axios.post('/create-item', {text: createField.value}).then(function(response) {
+       // create HTML item
+       document.getElementById('item-list').insertAdjacentHTML('beforeend', itemTemplate(response.data))
+        createField.value = ''
+        createField.focus()
+      }).catch(function() {
+         console.log('Please try again later.')
+   })
+})
+
+document.addEventListener('click', function(e) {
+  // delete feature
+
+  if(e.target.classList.contains('delete-me')) {
+      if(confirm('Do your really want to delete this item?')) {
+     axios.post('/delete-item', {id: e.target.getAttribute('data-id')}).then(function() {
+    e.target.parentElement.parentElement.remove()
+     }).catch(function() {
+         console.log('Please try again later.')
+     })
+    }
+  }
+
+
+  // update feature
+if(e.target.classList.contains('edit-me')) {
+
+   let userInput = prompt('Enter your desire new text', e.target.parentElement.parentElement.querySelector('.item-text').innerHML)
+   
+   if(userInput) {
+    axios.post('/update-item', {text: userInput, id: e.target.getAttribute('data-id')}).then(function() {
+        // do something here interesting in the next  video
+        e.target.parentElement.parentElement.querySelector('.item-text').innerHML = userInput
+       }).catch(function() {
+           console.log('Please try again later.')
+       })
+   }
+}
+
+})
--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 let express = require('express');
+// require the mongodb package after installing mongodb
+let mongodb = require('mongodb')
 
 let app = express();
 
+// define a dbase varible
+let db
+
+
+app.use(express.static('public'))
+
+let connectString = 'mongodb+srv://todoAppUser:edward1996@cluster0.x8oqf.mongodb.net/TodoApp?retryWrites=true&w=majority'
+mongodb.connect(connectString, {useNewUrlParser: true}, function(err, client) {
+  db = client.db()
+  app.listen(3000)
+})
+
+
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', function(req, res) {
+
+// password encrytion
+function passwordProtected(req, res, next) {
+   res.set('WWW-Authenticate', 'Basic realm="Simple Todo app"')
+  
+   console.log(req.headers.authorization)
+   if(req.headers.authorization == 'Placeholder') {
+    next()
+   } else {
+    res.status(401).send('Authenciation required')
+   }
+}
+
+app.get('/', passwordProtected, function(req, res) {
+
+  // reading data from db
+  db.collection('items').find().toArray(function(err, items) {
+    //console.log(items)
 
     res.send(`
     
@@ -21,52 +54,56 @@ app.get('/', function(req, res) {
     <h1 class="display-4 text-center py-1">To-Do App</h1>
     
     <div class="jumbotron p-3 shadow-sm">
-      <form action='/create-item' method='POST'>
+      <form action='/create-item' method='POST' id='create-form'>
         <div class="d-flex align-items-center">
-          <input name='item' autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+          <input id='create-field' name='item' autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
           <button class="btn btn-primary">Add New Item</button>
         </div>
       </form>
     </div>
     
-    <ul class="list-group pb-5">
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #1</span>
-        <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-      </li>
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #2</span>
-        <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-      </li>
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #3</span>
-        <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-      </li>
+    <ul class="list-group pb-5" id='item-list'>
+     
     </ul>
     
   </div>
+
   
+ <script>
+ let items = ${JSON.stringify(items)}
+ </script>
+
+ <script src='/browser.js'></script>
+ <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+ 
 </body>
 </html>
 
     `)
+  })
+
+    
 })
 
 app.post('/create-item', function(req, res) {
-
-    console.log(req.body.item)
+   // console.log(req.body.item)
+   db.collection('items').insertOne({text: req.body.text}, function(err, info) {
+    res.json(info.ops[0])
+   })
     
-    res.send('Thank for submitting the form')
 })
 
+app.post('/update-item', function(req, res) {
+  //console.log(req.body.text)
+//res.send('Success')
+  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+    res.send('Success')
+  })
+})
 
-app.listen(3000)
+app.post('/delete-item', function(req, res) {
+  db.collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, function() {
+    res.send('You have successfully deleted one item.')
+  })
+})
+
